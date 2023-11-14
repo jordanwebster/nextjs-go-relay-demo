648f6ea9cfe0513650c52427bfbb0de5c05c7cb2
Jordan Webster <5429117+jordanwebster@users.noreply.github.com>
2023-11-14 17:06:35 +0000

    Add ability to create new task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,5 +1,5 @@
-import { useTransition } from "react";
-import { usePaginationFragment } from "react-relay";
+import { useCallback, useState } from "react";
+import { useMutation, usePaginationFragment } from "react-relay";
 import { graphql } from "relay-runtime";
 import { TaskListFragment$key } from "../../__generated__/TaskListFragment.graphql";
 import Task from "./Task";
@@ -13,6 +13,7 @@ const TaskListFragment = graphql`
   ) {
     todos(after: $cursor, first: $count)
       @connection(key: "TaskListFragment_todos") {
+      __id
       pageInfo {
         startCursor
         hasNextPage
@@ -27,28 +28,77 @@ const TaskListFragment = graphql`
   }
 `;
 
+const TaskListAddTaskMutation = graphql`
+  mutation TaskListAddTaskMutation(
+    $connections: [ID!]!
+    $input: CreateTodoInput!
+  ) {
+    createTodo(input: $input)
+      @appendNode(connections: $connections, edgeTypeName: "TodoEdge") {
+      ...TaskFragment
+    }
+  }
+`;
+
 type Props = {
   tasks: TaskListFragment$key;
 };
 
 export default function TaskList({ tasks }: Props) {
-  const [isPending, startTransition] = useTransition();
-  const { data, loadNext } = usePaginationFragment(TaskListFragment, tasks);
+  const [taskTitle, setTaskTitle] = useState("");
+  const { data, loadNext, hasNext, isLoadingNext } = usePaginationFragment(
+    TaskListFragment,
+    tasks,
+  );
+  const [addTask, _isAddTaskMutationInFlight] = useMutation(
+    TaskListAddTaskMutation,
+  );
+
+  // TODO: Remove any typing
+  const onChange = useCallback(
+    (event: any) => {
+      setTaskTitle(event.target.value);
+    },
+    [setTaskTitle],
+  );
+
+  const onSubmit = useCallback(
+    (event: any) => {
+      event.preventDefault();
+      addTask({
+        variables: {
+          input: {
+            text: taskTitle,
+          },
+          connections: [data.todos.__id],
+        },
+      });
+      setTaskTitle("");
+    },
+    [taskTitle, setTaskTitle, addTask, data.todos],
+  );
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input placeholder="Create new task" onChange={onChange} />
+        <button type="submit">Create</button>
+      </form>
       <ul>
-        {data.todos.edges.map((edge) => (
-          <li key={edge.node.id}>
-            <Task task={edge.node} />
-          </li>
-        ))}
+        {data.todos.edges?.map(
+          (edge) =>
+            edge?.node && (
+              <li key={edge.node.id}>
+                <Task task={edge.node} />
+              </li>
+            ),
+        )}
       </ul>
       <button
         type="button"
-        disabled={isPending}
-        onClick={() => {
-          startTransition(() => loadNext(5));
-        }}
+        disabled={isLoadingNext || !hasNext}
+        onClick={() => loadNext(5)}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-slate-50 disabled:text-slate-500"
       >
         Load More
       </button>
